d5d9f0b1871a7342290a52d11c79b316fc2cf476
wusfen <404315887@qq.com>
2014-12-06 18:56:07 +0800

    涂层可用图片

--- wu.guagua.js
+++ wu.guagua.js
@@ -1,9 +1,15 @@
 /**
+
+by wushufen
+2014.11.25
+wusfun@foxmail.com
+
 wu.guagua({
 	el: document.getElementById('guaguaCt'),
+	cover: 'img.png',
 	callback: function(pc){
 		document.title = pc;
-		if (pc > 38) {
+		if (pc > 40) {
 			document.body.style.background = 'green'
 		}
 	}
@@ -13,6 +19,7 @@ wu.guagua({
 
 	wu.guagua = function(options){
 		var el = options.el,
+			cover = options.cover,
 			callback = options.callback;
 
 		var width = el.clientWidth;
@@ -36,53 +43,62 @@ wu.guagua({
 
 		// 涂层
 		var ctx = canvas.getContext('2d');
-		ctx.fillStyle = '#888';
+		ctx.fillStyle = '#A3A3A3';
 		ctx.fillRect(0,0,width,height);
-		ctx.globalCompositeOperation = 'destination-out';//关键
-		// ctx.fillRect(100,10,10,10);
+
+		// 图片涂层
+		if (cover) {
+			var coverImg = new Image;
+			coverImg.src = cover;
+			coverImg.onload = function(){
+				ctx.drawImage(coverImg,0,0);
+			};
+		};
 
 		// 刮刮
 		var gua = function(e){
 			e.preventDefault();
+			ctx.globalCompositeOperation = 'destination-out';//关键
+			// ctx.fillRect(100,10,10,10);
 			// console.log(e);
 
-            if(e.changedTouches){
-                e=e.changedTouches[e.changedTouches.length-1];
-            }
-            var x = (e.clientX + document.body.scrollLeft || e.pageX) - el.offsetLeft || 0,
-                y = (e.clientY + document.body.scrollTop || e.pageY) - el.offsetTop || 0;
+			if(e.changedTouches){
+				e=e.changedTouches[e.changedTouches.length-1];
+			}
+			var x = (e.clientX + document.body.scrollLeft || e.pageX) - el.offsetLeft || 0,
+				y = (e.clientY + document.body.scrollTop || e.pageY) - el.offsetTop || 0;
 			// console.log(x,y);
 
-            with(ctx) {
-                arc(x, y, 15, 0, Math.PI * 2);
-                fill();
-            }
+			ctx.arc(x, y, 15, 0, Math.PI * 2);
+			ctx.fill();
 
-            // 回调
-            callback(getTransparentPercent());
-		}
+			// 回调
+			callback&&callback(getTransparentPercent());
+		};
 
 		// 透明百分比
 		var getTransparentPercent = function() {
-		    var imgData = ctx.getImageData(0, 0, width, height),
-		        pixles = imgData.data,
-		        transPixs = [];
-		    for (var i = 0, j = pixles.length; i < j; i += 4) {
-		        var a = pixles[i + 3];
-		        if (a < 128) {
-		            transPixs.push(i);
-		        }
-		    }
-		    return (transPixs.length / (pixles.length / 4) * 100).toFixed(2);
-		}
+			var imgData = ctx.getImageData(0, 0, width, height),
+				pixles = imgData.data,
+				transPixs = [];
+			for (var i = 0, j = pixles.length; i < j; i += 4) {
+				var a = pixles[i + 3];
+				if (a < 128) {
+					transPixs.push(i);
+				}
+			}
+			return (transPixs.length / (pixles.length / 4) * 100).toFixed(2);
+		};
 
 		// 注册事件
 		canvas.addEventListener('touchmove', gua);
 		canvas.addEventListener('mousemove', gua);
 
-		// window.canvas = canvas;
+		return {
+			ctx: ctx
+		};
 
-	}
+	};
 
 	window.wu = wu;
 }(window.wu||{})
